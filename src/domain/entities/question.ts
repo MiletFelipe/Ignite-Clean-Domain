d2d503dd23a5a28a6e6975740641c683612feed3
Felipe Milet <felipemilet@hotmail.com>
2023-10-16 17:56:16 -0300

    Abstraindo criação de entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,28 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
     title: string
     content: string
     slug: Slug
-    authorId: string
+    createAt: Date
+    updateAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
-  
+  static create(
+    props: Optional<QuestionProps, 'createAt'>,
+     id?: UniqueEntityID
+     ) {
+    const question = new Question({
+        ...props,
+        createAt: new Date(),
+    }, id)
+
+    return question
+  }
 }
